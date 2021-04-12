f98398c0338c45fff3a9f626d9bf5ee58353aaa4
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-04-12 14:00:08 +0200

    :sparkles: return function error in the body

--- src/routes/endpoint.ts
+++ src/routes/endpoint.ts
@@ -1,25 +1,43 @@
 import supabase from "$lib/db";
 
 export async function post() {
-  const { data, error } = await supabase.auth.api.signInWithEmail("dummy@dummy.com", "password");
-  console.log({ data, error });
-  return {
-    body: { data, error }
-  };
+  try {
+    const { data, error } = await supabase.auth.api.signInWithEmail("dummy@dummy.com", "password");
+    console.log({ data, error });
+    return {
+      body: { data, error }
+    };
+  } catch (e) {
+    return {
+      body: { e }
+    };
+  }
 }
 
 export async function put() {
-  const { data, error } = await supabase.auth.signIn({ email: "dummy@dummy.com", password: "password" });
-  console.log({ data, error });
-  return {
-    body: { data, error }
-  };
+  try {
+    const { data, error } = await supabase.auth.signIn({ email: "dummy@dummy.com", password: "password" });
+    console.log({ data, error });
+    return {
+      body: { data, error }
+    };
+  } catch (e) {
+    return {
+      body: { e }
+    };
+  }
 }
 
 export async function get() {
-  const { data, error } = await supabase.from("messages").select();
-  console.log({ data, error });
-  return {
-    body: { data, error }
-  };
+  try {
+    const { data, error } = await supabase.from("messages").select();
+    console.log({ data, error });
+    return {
+      body: { data, error }
+    };
+  } catch (e) {
+    return {
+      body: { e }
+    };
+  }
 }
