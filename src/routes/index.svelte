a1ed916101043ec2b72ea4ea25ff701f89e9d39d
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-04-12 13:56:17 +0200

    :sparkles: fetch the endpoints

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,27 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+async function request(method: "POST" | "GET" | "PUT") {
+  const data = await fetch("/endpoint", {
+    method
+  });
+  const json = await data.json()
+  response = JSON.stringify(json) ;
+}
+
+let response = "Waiting for request...";
 </script>
 
 <svelte:head>
-	<title>Hello world!</title>
+  <title>Hello world!</title>
 </svelte:head>
 
 <main>
-	<h1>Hello world!</h1>
+  <div>
+    <h1>Response</h1>
+    <p>{response}</p>
+  </div>
 
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+  <button on:click={() => request("POST")}>supabase.auth.api.signInWithEmail</button>
+  <button on:click={() => request("PUT")}>supabase.auth.signIn</button>
+  <button on:click={() => request("GET")}>supabase.from</button>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
